08ea1412cecffe960664a49d6b164cd953c558ba
lschpmn <luis@lmc-productions.com>
2016-04-27 22:55:39 -0500

    Cleaned up server index file

--- index.js
+++ index.js
@@ -3,24 +3,16 @@
 const http = require('http');
 const path = require('path');
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+let mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
   const indexPath = path.join(__dirname, 'public/index.html');
   const host = process.env.host;
@@ -35,19 +27,10 @@ app.on('ready', function() {
     frame: false
   });
   
-  // and load the index.html of the app.
   mainWindow.loadURL(host == null ? indexPath : host);
   
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-  
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  if(host) mainWindow.webContents.openDevTools();
   
   //server listening to front-end
   //using this, instead of built in electron way because of issues with webpack+electron
@@ -56,7 +39,7 @@ app.on('ready', function() {
   
   server.on('request', (req, res) => {
     if(req.url === '/close') {
-      process.exit(0);
+      app.quit();
     }
     
     if(req.url === '/minimize') {
