a38f6eb30de50cc39b2f3e7386b9744789be96fa
lschpmn <luis@lmc-productions.com>
2019-02-14 01:33:23 -0600

    Removed unneeded dependency

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 import './index.html';
 
